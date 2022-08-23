d11755bb49371960b8f7de7e948dd3865c8207f4
simatiga <parlor.simatiga@gmail.com>
2022-08-23 21:46:53 +0900

    8

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -109,18 +109,4 @@ export default {
   padding: 20px;
 }
 
-
-
-
-  /* 반응형 최대크기 */
-  #app { margin:0 auto; width:750px; min-height:100%; padding-top:71px; border-left:1px #D8D8D8 solid; border-right:1px #D8D8D8 solid; positon:relative; text-align:center; }
-
-  /* 여백제거 + 드래그방지 */
-  body{ padding:0; margin:0; -ms-user-select: none; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; user-select: none; }
-
-  /* 반응형 시작 */
-  @media(max-width:750px){
-    #app{ width:100%; border-left:0; border-right:0; }
-  }
-
 </style>
