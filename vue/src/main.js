cdfc77b92e38e849c6a54c6132318bf64d912ae5
simatiga <parlor.simatiga@gmail.com>
2022-08-23 21:52:52 +0900

    9

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,14 +5,7 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-// import Vue from 'vue'
 // import Router from './router.js'       // 라우터 설정 js를 저장했던 경로를 지정.(여기서는 ./router.js)
-
-
-// Vue.config.productionTip = false
-// app.use(Router);
-// app.mount('#app');
-// Vue.use(BootstrapVue)
 
 // createApp(App).mount('#app')
 const app = createApp(App)
